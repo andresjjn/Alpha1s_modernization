90b20105ca53dddac6d4c06628aeade495031240
Andres Felipe Jejen Tabares <42705768+andresjjn@users.noreply.github.com>
2024-05-29 18:34:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from alpha1s import Alpha1s
 from alpha1s.constants import walk, init
 from time import sleep
 
+
 def main():
     robot = Alpha1s()
     print(robot.battery())
     robot.led_handler(False)
-    robot.servo_write_all(init)
-    #for i in range (0, len(walk)):
-        #robot.servo_write_all(walk[i])
-        #sleep(0.5)
+    for i in range(0, len(walk)):
+        robot.servo_write_all(walk[i])
+        sleep(0.5)
 
 
 if __name__ == '__main__':
